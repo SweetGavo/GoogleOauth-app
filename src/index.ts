1e1366de1b55a66599dac064f695d8e10d9933c7
Gavin Emuveyan <longgaving@gmail.com>
2023-08-09 16:51:05 +0100

    jwt auth

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from "express"
+import http from "http"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import compression from "compression"
+import cors from "cors"
+import mongoose from "mongoose"
+import router from "./router"
+
+const app = express();
+
+app.use(cors({
+    credentials : true,
+}))
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+app.get("/",(req:express.Request,res:express.Response)=>{
+res.send("API...");
+})
+
+
+server.listen(8080,()=>{
+    console.log("App server listening on http://localhost:8080/");
+    
+});
+
+const MONGODB_URL = 'mongodb+srv://gavin:allieantisnode@cluster1.pxqwsfi.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URL);
+
+mongoose.connection.on("error",(error:Error) => console.log(error))
+
+app.use('/',router());
